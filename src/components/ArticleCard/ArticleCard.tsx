6ad40577a5363e29413d9cfd63978a032df78e00
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2024-04-25 02:33:25 +0200

    add filter and select import

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -4,15 +4,15 @@ import { Card } from 'antd';
 import { Article } from '../../types.ts';
 
 interface ArticleCardProps {
-    article: Article|never|any;
+    article: Article | undefined;
     loading: boolean;
 }
 
-const ArticleCard: React.FC<ArticleCardProps> = ({ article ,loading }) => {
+const ArticleCard: React.FC<ArticleCardProps> = ({ article, loading }) => {
     return (
-        <Card title={article?.title?.deu} bordered={true} loading={loading}>
-            {/*<p>{article?.description}</p>*/}
-            {/*<p>{article?.author} - {article?.source?.name}</p>*/}
+        <Card title={article?.title} bordered={true} loading={loading}>
+            <p>{article?.description}</p>
+            <p>{article?.author} - {article?.source?.name}</p>
             <p><a href={article?.url} target="_blank" rel="noopener noreferrer">Read more...</a></p>
         </Card>
     );
